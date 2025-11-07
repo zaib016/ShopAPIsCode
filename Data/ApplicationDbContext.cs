a948a4d12ef313d883c3e5bbb884ce4d4e3d01d8
zaib016 <balochzaib016@gmail.com>
2025-11-07 17:39:38 +0500

    Add Data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAPIsCode.Models.Entities;
+
+namespace ShopAPIsCode.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
